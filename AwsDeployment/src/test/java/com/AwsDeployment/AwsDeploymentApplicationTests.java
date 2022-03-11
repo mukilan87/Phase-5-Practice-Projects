b29a35214ea16019fccd38af5a8ff55987fa2f26
mukilan87 <mukilnithin@gmail.com>
2022-03-11 10:35:15 +0530

    Aws Deployment

---
+++ AwsDeployment/src/test/java/com/AwsDeployment/AwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AwsDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
